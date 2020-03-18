78161aba29b3ab6dbb5f5a71e047b62d4fdc5ad9
Gulam Qasim <gfram@hotmail.co.uk>
2020-03-18 19:50:04 +0000

    Added Example String

--- String/String/Program.cs
+++ String/String/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace String
 {
@@ -10,6 +6,15 @@ namespace String
     {
         static void Main(string[] args)
         {
+            //We can use Console.WriteLine to display text on the users screen
+            Console.WriteLine("We use string to input text on the system!");
+            //We use ReadLine to stop the console from closing down on the user
+            Console.ReadLine();
+
+
+            //But we can declare variables
+
+
         }
     }
 }
